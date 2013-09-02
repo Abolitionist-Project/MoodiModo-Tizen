6c7008b5845bd9a9aacfe27f3b79f309541a82ab
Imara <tiana.imara@outlook.com>
2013-09-02 22:37:39 +0200

    MoodiModo V1.0

---
+++ js/databasemanager.js
@@ -0,0 +1,288 @@
+/**
+ * DBManager class for storing and retrieving data from, and managing, the shopping list application's database.
+ */
+var self = this;
+var moodData = new Array();
+
+self.createMoodsStatement = "CREATE TABLE IF NOT EXISTS moods (timestamp TEXT PRIMARY KEY NOT NULL UNIQUE, mood INTEGER)";
+
+self.insertMoodStatement = "INSERT OR REPLACE INTO moods VALUES (?,?)";
+
+self.selectSingleMoodStatement = "SELECT * FROM moods WHERE timestamp = ?";
+self.selectCountSingleMoodStatement = "SELECT COUNT(*) AS totalcount FROM moods WHERE mood = ?";
+self.selectAllMoodDataStatement = "SELECT * FROM moods";
+
+self.orderMoodsDesc = " ORDER BY timestamp DESC";
+self.orderMoodsAsc = " ORDER BY timestamp ASC";
+
+self.onError = function(tx, error) {
+    console.log("[ERR] DBmanager: "+error.message);
+};
+
+self.onSuccess = function(tx, result) {
+};
+
+self.createTables = function() {
+    self.db.transaction(function(tx) {
+        tx.executeSql(self.createMoodsStatement, [], self.onSuccess, self.onError);
+    });
+};
+
+self.insertMood = function(timestamp, mood) {
+    self.db.transaction(function(tx) {
+         tx.executeSql(self.insertMoodStatement, [timestamp, mood], self.onSuccess, self.onError);
+    });
+};
+
+self.selectAllMoodData = function(callback) {
+    self.db.transaction(function(tx) {
+         tx.executeSql(
+             self.selectAllMoodDataStatement,
+             [],
+             function(tx, result) {
+                 var dataset = result.rows;
+                 callback(dataset);
+             },
+             self.onError);
+    });
+};
+
+self.selectSingleMood = function(timestamp, callback) {
+    self.db.transaction(function(tx) {
+        tx.executeSql(
+            self.selectSingleMoodStatement,
+            [timestamp],
+            function(tx, result) {
+                var dataset = result.rows;
+                callback(dataset);
+            },
+            self.onError);
+   });
+};
+
+self.selectCountSingleMood = function(mood, callback) {
+    self.db.transaction(function(tx) {
+        tx.executeSql(
+            self.selectCountSingleMoodStatement,
+            [mood],
+            function(tx, result) {
+                var dataset = result.rows;
+                callback(dataset);
+            },
+            self.onError);
+   });
+};
+
+self.db = openDatabase("MoodiModoDB", "0.1", "MoodiModo Database", 2 * 1024 * 1024);
+self.createTables();
+console.log("database opened");
+
+/*function fillDatabase()
+{
+	console.log("tables created");
+	self.insertMood("1338585185539", 4);
+	self.insertMood("1338564915539", 2);
+	self.insertMood("1338564998539", 3);
+	self.insertMood("1338564998539", 3);
+	self.insertMood("1338865424539", 3);
+	self.insertMood("1923966654539", 3);
+	self.insertMood("1396564998539", 1);
+}*/
+
+/*self.selectSingleMood("1338564998539", function(callback)
+{
+	var book;
+    var booksNumber = callback.length;
+    for (i = 0; i < booksNumber; i++) 
+    {
+       book = callback.item(i);
+       alert("timestamp: " + book.timestamp + ", mood: " + book.mood);
+    }
+});
+
+self.selectCountSingleMood(3, function(callback)
+{
+	var book;
+    var booksNumber = callback.length;
+    for (i = 0; i < booksNumber; i++) 
+    {
+       book = callback.item(i);
+       console.log("single selected mood count mood 3: " + book.totalcount);
+    }
+});
+*/
+
+function getMoodData()
+{
+	self.selectAllMoodData(function(callback)
+	{
+		var book;
+	    var booksNumber = callback.length;
+	    for (i = 0; i < booksNumber; i++) 
+	    {
+	       book = callback.item(i);
+	       moodData[i] = book;
+	    }
+	});
+	return moodData;
+}
+
+function getBarChartData()
+{
+	var sui=0;
+	var sad=0;
+	var ok=0;
+	var hap=0;
+	var ecs=0;
+	
+	var mood;
+    var moodsNumber = moodData.length;
+    for (i = 0; i < moodsNumber; i++) 
+    {
+       mood = moodData[i];
+       if(mood.mood == 0)
+       {
+    	   sui=sui+1;
+       }
+       else if(mood.mood == 1)
+       {
+    	   sad=sad+1;
+       }
+       else if(mood.mood == 2)
+       {
+    	   ok=ok+1;
+       }
+       else if(mood.mood == 3)
+       {
+    	   hap=hap+1;
+       }
+       else if(mood.mood == 4)
+       {
+    	   ecs=ecs+1;
+       }
+    }
+    
+    barChartData[0] = parseInt(sui, 10);
+    barChartData[1] = parseInt(sad, 10);
+    barChartData[2] = parseInt(ok, 10);
+    barChartData[3] = parseInt(hap, 10);
+    barChartData[4] = parseInt(ecs, 10);
+}
+
+function getLineChartData()
+{
+	var mood;
+    var moodsNumber = moodData.length;
+    for (i = 0; i < moodsNumber; i++) 
+    {
+       mood = moodData[i];
+       if(mood.mood == 0)
+       {
+    	   lineChartData[i] = 0;
+       }
+       else if(mood.mood == 1)
+       {
+    	   lineChartData[i] = 1;
+       }
+       else if(mood.mood == 2)
+       {
+    	   lineChartData[i] = 2;
+       }
+       else if(mood.mood == 3)
+       {
+    	   lineChartData[i] = 3;
+       }
+       else if(mood.mood == 4)
+       {
+    	   lineChartData[i] = 4;
+       }
+    }
+}
+
+
+/*function setBarChartData()
+{
+	self.selectAllMoodData(function(callback)
+	{
+		//Put all moods in data array
+		//Create 2 seperate functions: getBarChartData & getLineChartData
+		var sui=0;
+		var sad=0;
+		var ok=0;
+		var hap=0;
+		var ecs=0;
+		
+		var book;
+	    var booksNumber = callback.length;
+	    for (i = 0; i < booksNumber; i++) 
+	    {
+	       book = callback.item(i);
+	       if(book.mood == 0)
+	       {
+	    	   sui=sui+1;
+	    	   console.log("count mood 0: " + sui);
+	       }
+	       else if(book.mood == 1)
+	       {
+	    	   sad=sad+1;
+	    	   console.log("count mood 1: " + sad);
+	       }
+	       else if(book.mood == 2)
+	       {
+	    	   ok=ok+1;
+	    	   console.log("count mood 2: " + ok);
+	       }
+	       else if(book.mood == 3)
+	       {
+	    	   hap=hap+1;
+	    	   console.log("count mood 3: " + hap);
+	       }
+	       else if(book.mood == 4)
+	       {
+	    	   ecs=ecs+1;
+	    	   console.log("count mood 4: " + ecs);
+	       }
+	    }
+	    
+	    var chartData = [];
+	    chartData[0] = parseInt(sui, 10);
+	    chartData[1] = parseInt(sad, 10);
+	    chartData[2] = parseInt(ok, 10);
+	    chartData[3] = parseInt(hap, 10);
+	    chartData[4] = parseInt(ecs, 10);
+		return chartData;
+		console.log("barChartData DB: " + data[3]);
+	});
+}*/
+
+function saveMood(mood, manually)
+{
+	var timestamp = new Date().getTime() / 1000;
+	self.insertMood(timestamp, mood);
+	console.log("new mood stored with timestamp: " + timestamp + " and moodId: " + mood);
+	
+	if(manually == "true" || manually == true)
+	{
+		alert("Your reported mood has been saved.");
+		parent.history.back();
+	}
+	else
+	{
+		tizen.application.getCurrentApplication().exit();
+	}
+	
+}
+
+/*function saveMood(mood)
+{
+	var timestamp = Math.round(new Date().getTime() / 1000);
+	self.insertMood(timestamp, mood);
+	console.log("new mood stored with timestamp: " + timestamp + " and moodId: " + mood);
+	tizen.application.getCurrentApplication().exit();
+}*/
+
+$(document).delegate('#home', 'pagebeforeshow', function() {
+	console.log("db manager pagebeforeshow");
+	getMoodData();
+	console.log("db manager moods obtained from db");
+});
---
+++ js/main.js
@@ -0,0 +1,249 @@
+
+//Mood Notification
+var appControl = new tizen.ApplicationControl(
+		"http://tizen.org/appcontrol/operation/report_mood",
+		null);
+
+var notificationDict = {
+		content : "Click here to report your mood",
+		iconPath : "images/image1.jpg",
+		vibration : true,
+		appControl : appControl,
+};
+var moodNotification = new tizen.StatusNotification("SIMPLE",
+         "MoodiModo", notificationDict);
+var historyData = new Array();
+
+//--------------radiobuttons--------------//
+var getCheckedValue = function(radioObj) {
+	if(!radioObj)
+		return "";
+	var radioLength = radioObj.length;
+	if(radioLength == undefined)
+		if(radioObj.checked)
+		{
+			return radioObj.value;
+		}	
+		else
+		{
+			return "";
+		}
+	for(var i = 0; i < radioLength; i++) {
+		if(radioObj[i].checked) {
+			return radioObj[i].value;
+		}
+	}
+	return "";
+}
+
+function setCheckedValue(radioObj, newValue) {
+	console.log("setCheckedValue() called");
+
+	for(var i = 0; i < 5; i++) {
+		console.log("in for loop : " + i);
+		if(radioObj[i].value == newValue.toString()) {
+			setMoodNotificationInterval(radioObj[i].value);
+			$("#radio-" + i).prop("checked", true).checkboxradio("refresh");
+			console.log("checkedRadio: " + radioObj[i].value);
+			console.log("moodNotificationInterval: " + getMoodNotificationInterval());
+		}
+		else 
+		{
+			$("#radio-" + i).prop("checked", false).checkboxradio("refresh");
+		}
+	}
+	console.log("end");
+}
+
+//-------MoodNotification interval-------//
+function getMoodNotificationInterval()
+{
+	if(localStorage.getItem("moodNotificationInterval") != null)
+	{
+		return localStorage.getItem("moodNotificationInterval");
+	}
+	else
+	{
+		localStorage.setItem("moodNotificationInterval", 2);
+		return localStorage.getItem("moodNotificationInterval");
+	}
+}
+
+function setMoodNotificationInterval(value)
+{
+	localStorage.setItem("moodNotificationInterval", value);
+	setMoodAlarm();
+}
+
+$(document).delegate('#settings_moodNotification', 'pageshow', function() {
+	console.log("settings moodnotifi pageshow called");
+	setCheckedValue(document.forms['notificationOptionsForm'].elements['notificationOption'], getMoodNotificationInterval());
+});
+
+function fillHistory()
+{
+	//getMoodData();
+	console.log("mooddata length: " + historyData.length);
+
+	for(var i = 0; i < historyData.length; i++)
+	{
+		console.log("in for");
+		addElementToHistory(historyData[i], i);
+	}
+
+	/*console.log("in for");
+	document.getElementById("historyList").innerHTML += "<li id='0' class='ui-li-has-multiline'><a href='settings_moodNotification.html'>" +
+	"Ask for your mood <span class='ui-li-text-sub'>Every three hours</span> <div data-role='button' data-inline='true' data-icon='arrow-r' " +
+	"data-style='circle' href='settings_moodNotification.html'></div></a></li>";*/
+}
+
+function addElementToHistory(mood, i)
+{
+	console.log("adding element: " + mood.timestamp);
+	var moodName;
+	var moodDate = parseInt(mood.timestamp*1000, 10);
+	console.log("mooddate: " + moodDate);
+	var date = new Date(moodDate);
+
+	console.log("mooddate offset: " + date.getTimezoneOffset());
+	var dateString = dateToDMYHMS(date);
+	var moodName = new Array(5);
+	moodName[0]="Suicidal";
+	moodName[1]="Sad";
+	moodName[2]="OK";
+	moodName[3]="Happy";
+	moodName[4]="Ecstatic";
+	
+	moodName = moodName[mood.mood];
+	$('#historyList').append("<li id='" + i + "' class='ui-li-has-multiline'>" +
+			moodName + "<span class='ui-li-text-sub'>" + dateString + "</span></div></li>");
+	
+	
+//	document.getElementById("historyList").innerHTML += "<li id='" + i + "' class='ui-li-has-multiline'><a href='settings_moodNotification.html'>" +
+//	moodName + "<span class='ui-li-text-sub'>" + dateString + "</span></div></a></li>";
+	//" " + date.toLocaleTimeString() + 
+}
+
+
+function dateToDMYHMS(date) {
+	var weekday=new Array(7);
+	weekday[0]="Sunday";
+	weekday[1]="Monday";
+	weekday[2]="Tuesday";
+	weekday[3]="Wednesday";
+	weekday[4]="Thursday";
+	weekday[5]="Friday";
+	weekday[6]="Saturday";
+	
+	var dayName = weekday[date.getDay()];
+	var hour = date.getHours();
+	var minutes = date.getMinutes();
+	var seconds = date.getSeconds();
+
+	//TODO check if < 9, if so: add 0
+	return dayName + ", " + date.toLocaleDateString() + " " + hour + ":" + minutes + ':' + seconds;
+	
+}
+/*
+function addElementToHistory(mood, i)
+{
+	var moodName;
+	var moodDate = new Date();
+	moodDate.setSeconds(mood.timestamp, 0);
+
+	var dateString = dateToDMYHSM(moodDate);
+	
+	if(mood.mood == 0)
+	{
+		moodName = "Suicidal";
+	}
+	if(mood.mood == 1)
+	{
+		moodName = "Sad";
+	}
+	if(mood.mood == 2)
+	{
+		moodName = "OK";
+	}
+	if(mood.mood == 3)
+	{
+		moodName = "Happy";
+	}
+	if(mood.mood == 4)
+	{
+		moodName = "Ecstatic";
+	}
+	document.getElementById("historyList").innerHTML += "<li id='" + i + "' class='ui-li-has-multiline'><a href='settings_moodNotification.html'>" +
+	moodName + "<span class='ui-li-text-sub'>" + dateString + "</span></div></a></li>";
+}
+ */
+//------------------init------------------//
+
+var init = function() {
+	$('div[data-role="page"]:first .ui-btn-back').bind("click",
+			function(event) {
+				var currentApp = tizen.application.getCurrentApplication();
+				currentApp.exit();
+			});
+	
+	document.addEventListener('tizenhwkey', function(e) 
+		    {
+		    var activePage = $('.ui-page-active').attr('id');
+				if (e.keyName === 'back') 
+				{
+					if(activePage === 'home')
+					{
+						tizen.application.getCurrentApplication().exit();
+					}
+					else 
+					{
+						parent.history.back();
+					}
+				}
+		        else if (e.keyName === 'menu') 
+		        {
+		           console.log('in the menu yay');
+		        }
+			});
+	console.log("init() called");
+	
+	tizen.alarm.removeAll();
+	var alarms = tizen.alarm.getAll();
+	console.log(alarms.length + " alarms present in the storage.");
+	/*try
+	{	
+		console.log("app id: " + tizen.application.getCurrentApplication().appInfo.id);
+		tizen.notification.post(moodNotification);
+		var alarms = tizen.alarm.getAll();
+		
+		console.log(alarms.length + " alarms present in the storage.");
+	}
+    catch (e)
+    {
+        console.log("Exception: " + e);
+    }*/
+	console.log("storage: " + getMoodNotificationInterval());
+	console.log("moodNotificationInterval: " + getMoodNotificationInterval());	
+
+};
+$( "#historyList" ).listview({
+	   create: function(event, ui) {  }
+	});
+
+$(document).bind('pageinit', init);
+
+//----------------------------------------//
+$(document).delegate('#history', 'pagecreate', function() {
+	//historyData = getMoodData();
+	//fillHistory();
+});
+
+$(document).delegate('#history', 'pageshow', function() {
+	historyData = getMoodData();
+	$("#historyList").empty();
+	fillHistory();
+	$('#historyList').listview('refresh');
+});
+
+
+//TODO fillHistory() on page init/beforeshow without losing css...
